b328d7276654ff68dffa567ab86f5e02fce166d0
denisetiya <denisetiya009@gmail.com>
2026-08-18 17:26:00 +0700

    feat: add dismissible Alert component with ink:alert:dismiss event

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { default as Button } from "./components/Button.astro";
 export { default as Badge } from "./components/Badge.astro";
+export { default as Alert } from "./components/Alert.astro";
