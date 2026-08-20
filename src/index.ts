a3d4a0c74a62bbd8ccbf65042d6e3135a004c7e3
denisetiya <denisetiya009@gmail.com>
2026-08-20 13:09:00 +0700

    feat: add Container and VisuallyHidden with components index page

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { default as Button } from "./components/Button.astro";
 export { default as Badge } from "./components/Badge.astro";
 export { default as Alert } from "./components/Alert.astro";
+export { default as Container } from "./components/Container.astro";
+export { default as VisuallyHidden } from "./components/VisuallyHidden.astro";
